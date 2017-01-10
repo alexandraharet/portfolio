5fe3899db89032821076e8e4b2ec7f631d9a54a0
Alexandra Haret <alexandra.haret@gmail.com>
2017-01-10 13:01:59 +0000

    spellchecked content, removed unused css, added width & height to images that were missing these, to speed up page load

--- js/app.js
+++ js/app.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
 
-var addZoomInAndButton = function(myClass) {
-  $(myClass).prepend("<div class='buttonMore'>View Project</div>")
-  .hover(
-    function(){
-      $(this).find("img").addClass("zoom-in");
-    },
-    function(){
-      $(this).find("img").removeClass("zoom-in");
-    });
-}
+  var addZoomInAndButton = function(myClass) {
+    $(myClass).prepend("<div class='buttonMore'>View Project</div>")
+    .hover(
+      function(){
+        $(this).find("img").addClass("zoom-in");
+      },
+      function(){
+        $(this).find("img").removeClass("zoom-in");
+      });
+    }
 
-// $(".herotext").delay(500).animate({top: "+20vh", opacity: 1}, 1000);
+    $(".herotext").delay(500).addClass("show");
 
-$(".herotext").delay(500).addClass("show");
+    // WORKS
 
-// WORKS
+    var addStackIcons = function() {
+      var technologies = $(".technologies").children("div");
+      technologies.each(function(){
+        $(this).append("<img src='img/technologies/" + this.className + ".png' / class='tech-icons' alt='" + this.className + " icon' title='" + this.className + "'/>");
+      });
 
-var addStackIcons = function() {
-  var technologies = $(".technologies").children("div");
-  technologies.each(function(){
-    $(this).append("<img src='img/technologies/" + this.className + ".png' / class='tech-icons' alt='" + this.className + " icon' title='" + this.className + "'/>");
-  });
+    }
 
-}
+    // NEEDS FIXING
 
-// NEEDS FIXING
+    /*
 
-/*
-
-var addStackIcons = function() {
-  var technologies = [];
-  technologies = $(".technologies").children("div").attr("class").split(/\s+/);
-  console.log(technologies);
-  $.each(technologies, function(){
-  $(".technologies").children("div").append("<img src='../ah-landing-page/img/technologies/" + this + ".png' / class='tech-icons'>");
-  technologies = [];
-  console.log(technologies);
+    var addStackIcons = function() {
+    var technologies = [];
+    technologies = $(".technologies").children("div").attr("class").split(/\s+/);
+    console.log(technologies);
+    $.each(technologies, function(){
+    $(".technologies").children("div").append("<img src='../ah-landing-page/img/technologies/" + this + ".png' / class='tech-icons'>");
+    technologies = [];
+    console.log(technologies);
   });
 
 }
@@ -46,20 +44,20 @@ var addStackIcons = function() {
 
 /* NEEDS FIXING
 var addStackIcons = function() {
-  $.each(getProjectIDs(), function(){
-    var technologies = $(".technologies #" + this).children("div").attr("class").split(/\s+/);
-    console.log(technologies);
-    $.each(technologies, function(){
-    $(".technologies").children("div").append("<img src='../ah-landing-page/img/technologies/" + this + ".png' / class='tech-icons'>");
-    });
-  })
+$.each(getProjectIDs(), function(){
+var technologies = $(".technologies #" + this).children("div").attr("class").split(/\s+/);
+console.log(technologies);
+$.each(technologies, function(){
+$(".technologies").children("div").append("<img src='../ah-landing-page/img/technologies/" + this + ".png' / class='tech-icons'>");
+});
+})
 }
 
 var getProjectIDs = function () {
-  var projectIDs = [];
-  $.each($(".card"), function() {
-    if ($(this).prop("id")) projectIDs.push($(this).prop("id"));
-  });
+var projectIDs = [];
+$.each($(".card"), function() {
+if ($(this).prop("id")) projectIDs.push($(this).prop("id"));
+});
 }
 
 */
@@ -70,7 +68,7 @@ var getProjectIDs = function () {
 var timelineToCenter = function() {
   $(".middleLine").css({"background-position": "center"});
   if ($(".iconContainer").length > 0 ) $(".iconContainer").remove();
-    $(".timelineEvent").each(function() {
+  $(".timelineEvent").each(function() {
     $(this).append("<div class='iconContainer'> <img src='img/" + $(this).attr("id") + "-w.png' class='timelineIcon' /> </div>");
   })
 }
@@ -79,12 +77,12 @@ var timelineToLeft = function() {
   $(".timelineIcon").css({bottom: "0px", top: "-30px", "margin-top": "10px"});
 }
 
-var addTimelineArrows = function(isLeft) {
-$(".middleLine").css({
-  "visibility": "visible",
+var addTimelineIcons = function(isLeft) {
+  $(".middleLine").css({
+    "visibility": "visible",
   });
   if (isLeft)
-    timelineToLeft();
+  timelineToLeft();
   else timelineToCenter();
 }
 
@@ -93,41 +91,38 @@ if ($('.eventContent').length > 0 ) {
   $(window).on("load resize", function() {
     var isLeft = $("body").width() < 640;
     if (wasLeft != isLeft) {
-      addTimelineArrows(isLeft);
+      addTimelineIcons(isLeft);
       wasLeft = isLeft;
     }
   });
 };
 
-addTimelineArrows();
-
-// back to top button fade-in and fade-out
-
-// hide #back-top first
-  $("#back-top").hide();
-
-  // fade in #back-top
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('#back-top').fadeIn();
-      } else {
-        $('#back-top').fadeOut();
-      }
-    });
-
-    // scroll body to 0px on click
-    $('#back-top').click(function () {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 800);
-      return false;
-    });
-  });
-
 // end
 
+// back to top button fade-in and fade-out
+// hide #back-top first
+$("#back-top").hide();
 
+// fade in #back-top
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+
+  // scroll body to 0px on click
+  $('#back-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+});
+
+// end
 
 // Scrollmagic for animated timeline
 
@@ -137,42 +132,51 @@ $(function() {
   var scrollMagicController = new ScrollMagic.Controller();
 
   var animateTimeline = function() {
-  var offset = - $(window).height()/4.5; // TODO: recalculate offset on screen resize;
+    var offset = - $(window).height()/4.5; // TODO: recalculate offset on screen resize;
 
-  $('.timelineEvent').each(function() {
+    $('.timelineEvent').each(function() {
       var scene = new ScrollMagic.Scene({
-         triggerElement: this,
-         offset
-       }).setClassToggle(this, "show").addTo(scrollMagicController);
-       var triggerElement = scene.triggerElement();
-       $(triggerElement).css({
-         "-webkit-overflow-scrolling": "touch"
-       }).children().css({
-           "-webkit-transform": "translateZ(0px)",
-           "-webkit-transform": "translate3d(0,0,0)"
-         });
+        triggerElement: this,
+        offset
+      }).setClassToggle(this, "show").addTo(scrollMagicController);
+      var triggerElement = scene.triggerElement();
+      $(triggerElement).css({
+        "-webkit-overflow-scrolling": "touch"
+      }).children().css({
+        "-webkit-transform": "translateZ(0px)",
+        "-webkit-transform": "translate3d(0,0,0)"
       });
-    }
-   animateTimeline();
-  });
+    });
+  }
+  animateTimeline();
+});
 
-/* END */
+// end
 
+var getDayAndYear = function() {
+  var d = new Date();
+  var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  $("#currentyear").text(d.getFullYear());
+  $("#day").text(weekday[d.getDay()]);
+}
 
 
 addZoomInAndButton(".img-container");
 addStackIcons();
+getDayAndYear();
+addTimelineIcons();
+
 
 
 // vertical alignment to middle using CSS, for class .center
 /*
 $(window).resize(function() {
- $(".toMiddle").each(function() {
-   var wh = (($(this).parent().height()-$(this).height())/2)+'px';
-   $(this).css({
-       top: wh
-   });
- });
+$(".toMiddle").each(function() {
+var wh = (($(this).parent().height()-$(this).height())/2)+'px';
+$(this).css({
+top: wh
+});
+});
 
 
 }).resize(); */
